7729e17217d4b7fe73cd215d26ebec713850482a
Roberto Magno <roberto.magno@g.globo>
2021-10-08 12:04:20 -0300

    Solução Code Challenge 1

--- DataBinding/BindingDemo2/app/src/main/java/com/anushka/bindingdemo2/MainActivity.kt
+++ DataBinding/BindingDemo2/app/src/main/java/com/anushka/bindingdemo2/MainActivity.kt
@@ -5,26 +5,28 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
+import androidx.databinding.DataBindingUtil
+import com.anushka.bindingdemo2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var button: Button
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        button = findViewById(R.id.control_button)
-        button.setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.controlButton.setOnClickListener {
             startOrStopProgressBar()
         }
     }
 
     private fun startOrStopProgressBar() {
-        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        if (progressBar.visibility == View.GONE) {
-            progressBar.visibility = View.VISIBLE
-            button.text = "Stop"
-        } else {
-            progressBar.visibility = View.GONE
-            button.text = "Start"
+        binding.apply {
+            if (progressBar.visibility == View.GONE) {
+                progressBar.visibility = View.VISIBLE
+                controlButton.text = getString(R.string.btn_stop_txt)
+            } else {
+                progressBar.visibility = View.GONE
+                controlButton.text = getString(R.string.btn_start_txt)
+            }
         }
     }
 }
