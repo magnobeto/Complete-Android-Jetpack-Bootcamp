a9aa19129d37e7aec80433f0445aa7c82adc15e8
Roberto Magno <roberto.magno@g.globo>
2021-10-08 14:50:07 -0300

    Exemplo de Databinding com objetos

--- DataBinding/BindingDemo3/app/src/main/java/com/anushka/bindingdemo3/MainActivity.kt
+++ DataBinding/BindingDemo3/app/src/main/java/com/anushka/bindingdemo3/MainActivity.kt
@@ -10,9 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        val student =getStudent()
-        binding.nameText.text = student.name
-        binding.emailText.text = student.email
+        binding.student = getStudent()
     }
 
     private fun getStudent():Student{
