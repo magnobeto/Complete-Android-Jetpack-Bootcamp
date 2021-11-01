f848152344a23e8ead41f660fd91930f7efcafb7
Roberto Magno <roberto.magno@g.globo>
2021-11-01 12:03:27 -0300

    Criando um ViewModel customziado com o pattern ViewModelFactory

--- ViewModel/ViewModelDemo3/app/src/main/java/com/anushka/viewmodeldemo3/MainActivity.kt
+++ ViewModel/ViewModelDemo3/app/src/main/java/com/anushka/viewmodeldemo3/MainActivity.kt
@@ -10,11 +10,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainActivityViewModel
+    private lateinit var viewModelFactory: MainViewModelActivityFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModelFactory = MainViewModelActivityFactory(10)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
 
         showingTotalSumSetup()
 
--- ViewModel/ViewModelDemo3/app/src/main/java/com/anushka/viewmodeldemo3/MainActivityViewModel.kt
+++ ViewModel/ViewModelDemo3/app/src/main/java/com/anushka/viewmodeldemo3/MainActivityViewModel.kt
@@ -2,9 +2,13 @@ package com.anushka.viewmodeldemo3
 
 import androidx.lifecycle.ViewModel
 
-class MainActivityViewModel: ViewModel() {
+class MainActivityViewModel(startingValue: Int): ViewModel() {
     
     private var totalSum: Int = 0
+
+    init {
+        totalSum = startingValue
+    }
     
     fun sumNumberInput(number: Int) {
         totalSum += number
