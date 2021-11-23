a87cabbc2b2c25c17a46155fcfebafa112895223
Roberto Magno <roberto.magno@g.globo>
2021-11-23 17:07:08 -0300

    Criando navigation destinations

---
+++ Navigation_Architecture_Component/NavDemo1/app/src/main/java/com/example/navdemo1/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.navdemo1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.navdemo1.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+}
---
+++ Navigation_Architecture_Component/NavDemo1/app/src/main/java/com/example/navdemo1/SecondFragment.kt
@@ -0,0 +1,23 @@
+package com.example.navdemo1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.navdemo1.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+        return binding.root
+    }
+}
