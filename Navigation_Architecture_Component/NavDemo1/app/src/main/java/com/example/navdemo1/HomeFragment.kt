102213702228ae6bf8acf0fc12964da506363afd
Roberto Magno <roberto.magno@g.globo>
2022-01-04 16:41:19 -0300

    Configurando uma navigation action

--- Navigation_Architecture_Component/NavDemo1/app/src/main/java/com/example/navdemo1/HomeFragment.kt
+++ Navigation_Architecture_Component/NavDemo1/app/src/main/java/com/example/navdemo1/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.navdemo1.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -18,6 +19,9 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding.button.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment)
+        }
         return binding.root
     }
 }
