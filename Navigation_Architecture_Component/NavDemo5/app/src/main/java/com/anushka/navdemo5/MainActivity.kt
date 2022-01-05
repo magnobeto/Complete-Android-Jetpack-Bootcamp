6c4d734ddc56a1fae5cd11b95a40b9cc7be36251
Roberto Magno <roberto.magno@g.globo>
2022-01-05 12:48:31 -0300

    Criando nav_host e configurando tela inicial

--- Navigation_Architecture_Component/NavDemo5/app/src/main/java/com/anushka/navdemo5/MainActivity.kt
+++ Navigation_Architecture_Component/NavDemo5/app/src/main/java/com/anushka/navdemo5/MainActivity.kt
@@ -11,6 +11,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-
     }
 }
