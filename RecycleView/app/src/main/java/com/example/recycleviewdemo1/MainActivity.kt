0635dcb678603ab77bcd91776107dacbf56b45ac
Roberto Magno <roberto.magno@g.globo>
2022-01-05 21:19:53 -0300

    Criando um Layout Manager

--- RecycleView/app/src/main/java/com/example/recycleviewdemo1/MainActivity.kt
+++ RecycleView/app/src/main/java/com/example/recycleviewdemo1/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.recycleviewdemo1
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        my_recycle_view.setBackgroundColor(Color.GREEN)
+        my_recycle_view.layoutManager = LinearLayoutManager(this)
     }
 }
