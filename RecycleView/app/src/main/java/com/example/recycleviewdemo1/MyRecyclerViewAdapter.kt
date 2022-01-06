7106acae4ac823a5256a4432b21b524c426e1182
Roberto Magno <roberto.magno@g.globo>
2022-01-05 21:27:49 -0300

    Criando o Adapter e ViewHolder

---
+++ RecycleView/app/src/main/java/com/example/recycleviewdemo1/MyRecyclerViewAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.recycleviewdemo1
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyRecyclerViewAdapter: RecyclerView.Adapter<MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
+class MyViewHolder(val view : View): RecyclerView.ViewHolder(view) {
+
+}
