5a56c3cec684c9153c1038a90c16c95e438b6d9f
Roberto Magno Campos <roberto.magno@g.globo>
2022-04-10 20:25:08 -0300

    Configurando examplo Async/Await parallel decomposintion

---
+++ Coroutines Fundamentals/CoroutinesDemo2/app/src/main/java/com/example/coroutinesdemo2/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.coroutinesdemo2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnCount.setOnClickListener {
+            tvCount.text = count++.toString()
+        }
+        btnDownloadUserData.setOnClickListener {
+
+            CoroutineScope(Dispatchers.IO).launch {
+                downloadUserData()
+            }
+        }
+    }
+
+    private suspend fun downloadUserData() {
+        for (i in 1..200000) {
+            withContext(Dispatchers.Main) {
+                tvUserMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
+            }
+        }
+    }
+}
