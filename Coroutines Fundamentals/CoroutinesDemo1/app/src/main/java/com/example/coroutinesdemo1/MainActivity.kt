7577ca1d549c7402f32c0c2064a6f0d05c2c936b
Roberto Magno Campos <roberto.magno@g.globo>
2022-04-10 18:35:21 -0300

    Configurando exemplo inicial coroutines

---
+++ Coroutines Fundamentals/CoroutinesDemo1/app/src/main/java/com/example/coroutinesdemo1/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.coroutinesdemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnCount.setOnClickListener {
+            tvCount.text = count++.toString()
+        }
+        btnDownloadUserData.setOnClickListener {
+
+            CoroutineScope(Dispatchers.IO).launch {
+                downloadUserData()
+            }
+        }
+    }
+
+    private fun downloadUserData() {
+        for (i in 1..200000) {
+            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+        }
+    }
+}
