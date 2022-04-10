a5a7dfb5af5560f8cdda50476d192513ebdd4d7a
Roberto Magno Campos <roberto.magno@g.globo>
2022-04-10 20:31:09 -0300

    Simulando request com delay

--- Coroutines Fundamentals/CoroutinesDemo2/app/src/main/java/com/example/coroutinesdemo2/MainActivity.kt
+++ Coroutines Fundamentals/CoroutinesDemo2/app/src/main/java/com/example/coroutinesdemo2/MainActivity.kt
@@ -2,11 +2,10 @@ package com.example.coroutinesdemo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.coroutinesdemo1.R
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
@@ -14,23 +13,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btnCount.setOnClickListener {
-            tvCount.text = count++.toString()
-        }
-        btnDownloadUserData.setOnClickListener {
-
-            CoroutineScope(Dispatchers.IO).launch {
-                downloadUserData()
-            }
-        }
     }
 
-    private suspend fun downloadUserData() {
-        for (i in 1..200000) {
-            withContext(Dispatchers.Main) {
-                tvUserMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
-            }
-        }
+    private suspend fun getStock1(): Int {
+        delay(10000)
+        Log.i("Mytag", "stock 1 returned")
+        return 55000
+    }
+
+    private suspend fun getStock2(): Int {
+        delay(8000)
+        Log.i("Mytag", "stock 2 returned")
+        return 35000
     }
 }
