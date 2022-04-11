b3cf13759f8d9f0e2e4d1f782ecc596b9c4ef532
Roberto Magno Campos <roberto.magno@g.globo>
2022-04-11 18:10:19 -0300

    Executando exemplo inicial lifecycleScope

--- Coroutines Fundamentals/LifeCycleScopeDemo/app/src/main/java/com/example/lifecycledemo/MainActivity.kt
+++ Coroutines Fundamentals/LifeCycleScopeDemo/app/src/main/java/com/example/lifecycledemo/MainActivity.kt
@@ -2,17 +2,33 @@ package com.example.lifecycledemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import com.example.lifecycledemo.databinding.MainActivityBinding
 import com.example.lifecycledemo.ui.main.MainFragment
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+        }
+
+        lifecycleScope.launch {
+            delay(5000)
+            binding.progressBar.visibility = View.VISIBLE
+            delay(10000)
+            binding.progressBar.visibility = View.GONE
         }
     }
 }
