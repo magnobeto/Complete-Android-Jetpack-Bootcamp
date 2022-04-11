fe93c3a83434e2a7e8e25466aa612a2705e4b2fa
Roberto Magno Campos <roberto.magno@g.globo>
2022-04-11 16:05:44 -0300

    Utilizando boa prática onde o próprio escopo cancela a coroutine(viewModelScope)

--- Coroutines Fundamentals/ViewModelScopeDemo/app/src/main/java/com/example/viewmodelscopedemo/MainActivityViewModel.kt
+++ Coroutines Fundamentals/ViewModelScopeDemo/app/src/main/java/com/example/viewmodelscopedemo/MainActivityViewModel.kt
@@ -1,25 +1,14 @@
 package com.example.viewmodelscopedemo
 
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
 class MainActivityViewModel : ViewModel() {
 
-    private val myJob = Job()
-    private val myScope = CoroutineScope(Dispatchers.IO + myJob)
-
     fun getUserData(){
-        myScope.launch {
+        viewModelScope.launch {
             //write some code
         }
-
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        myJob.cancel()
     }
 }
