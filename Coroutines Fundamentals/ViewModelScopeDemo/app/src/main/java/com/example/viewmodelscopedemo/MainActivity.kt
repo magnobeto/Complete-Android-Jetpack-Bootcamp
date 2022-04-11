dd1b127eb65065349b8c5bc501f960f8b3e02bb4
Roberto Magno Campos <roberto.magno@g.globo>
2022-04-11 16:31:11 -0300

    Criando exemplo de boa prática usando viewmodel scope e livedata

--- Coroutines Fundamentals/ViewModelScopeDemo/app/src/main/java/com/example/viewmodelscopedemo/MainActivity.kt
+++ Coroutines Fundamentals/ViewModelScopeDemo/app/src/main/java/com/example/viewmodelscopedemo/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.viewmodelscopedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainActivityViewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainActivityViewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+        mainActivityViewModel.apply {
+            getUserData()
+            users.observe(this@MainActivity) { myUsers ->
+                myUsers.forEach {
+                    Log.i("MyTag", "Name is: ${it.name}")
+                }
+            }
+        }
     }
 }
--- Coroutines Fundamentals/ViewModelScopeDemo/app/src/main/java/com/example/viewmodelscopedemo/MainActivityViewModel.kt
+++ Coroutines Fundamentals/ViewModelScopeDemo/app/src/main/java/com/example/viewmodelscopedemo/MainActivityViewModel.kt
@@ -1,17 +1,25 @@
 package com.example.viewmodelscopedemo
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.viewmodelscopedemo.model.UserRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivityViewModel : ViewModel() {
 
     private val userRepository = UserRepository()
+    var users: MutableLiveData<List<User>> = MutableLiveData()
 
     fun getUserData(){
         viewModelScope.launch {
-            //write some code
+            lateinit var result: List<User>
+            withContext(Dispatchers.IO) {
+                result = userRepository.getUsers()
+            }
+            users.value = result
         }
     }
 }
