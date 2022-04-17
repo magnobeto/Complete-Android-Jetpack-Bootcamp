69933b0aa126cb244cabafdf134d32a51fd937f2
Roberto Magno Campos <roberto.magno@g.globo>
2022-04-17 15:02:45 -0300

    Fix Dispatcher

--- Coroutines Fundamentals/LiveDataBuilderDemo/app/src/main/java/com/example/livedatabuilderdemo/MainActivityViewModel.kt
+++ Coroutines Fundamentals/LiveDataBuilderDemo/app/src/main/java/com/example/livedatabuilderdemo/MainActivityViewModel.kt
@@ -3,11 +3,12 @@ package com.example.livedatabuilderdemo
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.example.livedatabuilderdemo.model.UserRepository
+import kotlinx.coroutines.Dispatchers
 
 class MainActivityViewModel : ViewModel() {
 
     private val userRepository = UserRepository()
-    var users = liveData {
+    var users = liveData(Dispatchers.IO) {
         val result = userRepository.getUsers()
         emit(result)
     }
