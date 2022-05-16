4cb4562d16a07a7e70cf867d7c87824d9b7a8e9f
Roberto Magno Campos <roberto.magno@g.globo>
2022-05-16 20:16:40 -0300

    Data Access Object Interface

---
+++ RoomDatabase/app/src/main/java/com/example/roomdemo1/db/SubscriberDAO.kt
@@ -0,0 +1,23 @@
+package com.example.roomdemo1.db
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SubscriberDAO {
+
+    @Insert
+    suspend fun insertSubscriber(subscriber: Subscriber): Long
+
+    @Update
+    suspend fun updateSubscriber(subscriber: Subscriber)
+
+    @Delete
+    suspend fun deleteSubscriber(subscriber: Subscriber)
+
+    @Query("DELETE FROM subscriber_data_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM subscriber_data_table")
+    fun getAllSubscribers(): Flow<List<Subscriber>>
+}
