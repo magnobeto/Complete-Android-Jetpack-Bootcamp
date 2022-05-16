32dbd9cdf88cd4370dc2b336094f3bd4fbfc39bd
Roberto Magno Campos <roberto.magno@g.globo>
2022-05-16 19:52:37 -0300

    Criando uma Entity Class(abstração de tabela dentro do banco) e importação de libs room

---
+++ RoomDatabase/app/src/main/java/com/example/roomdemo1/db/Subscriber.kt
@@ -0,0 +1,19 @@
+package com.example.roomdemo1.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "subscriber_data_table")
+data class Subscriber(
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "subscriber_id")
+    val id: Int,
+
+    @ColumnInfo(name = "subscriber_name")
+    val name: String,
+
+    @ColumnInfo(name = "subscriber_email")
+    val email: String
+)
