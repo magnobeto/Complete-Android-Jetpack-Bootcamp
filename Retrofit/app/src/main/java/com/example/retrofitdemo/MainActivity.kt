d993e531eaf6e05836e7c7e0cb7a1bcbed9d06b8
Roberto Magno <roberto.magno@g.globo>
2022-06-06 22:31:03 -0300

    View Data: Log

--- Retrofit/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ Retrofit/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.retrofitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import retrofit2.Response
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val retrofitService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(AlbumService::class.java)
+        val responseLiveDate: LiveData<Response<Albums>> = liveData {
+            val response = retrofitService.getAlbums()
+            emit(response)
+        }
+        responseLiveDate.observe(this) {
+            val albumsList = it.body()?.listIterator()
+            if (albumsList != null) {
+                while (albumsList.hasNext()) {
+                    val albumsItem = albumsList.next()
+                    Log.i("MyTag", albumsItem.title)
+                }
+            }
+        }
     }
 }
