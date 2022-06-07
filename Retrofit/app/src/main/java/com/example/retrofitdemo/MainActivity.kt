292d23acbeffdc2f3c6e4efceadb109bebdca85d
Roberto Magno <roberto.magno@g.globo>
2022-06-07 00:07:27 -0300

    Refactor

--- Retrofit/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ Retrofit/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -12,25 +12,22 @@ import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var retrofitService: AlbumService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        val retrofitService = RetrofitInstance
+        retrofitService = RetrofitInstance
             .getRetrofitInstance()
             .create(AlbumService::class.java)
 
-        val pathResponse: LiveData<Response<AlbumsItem>> = liveData {
-            val response = retrofitService.getAlbum(3)
-            emit(response)
-        }
-        pathResponse.observe(this) {
-            val title = it.body()?.title
-            Toast.makeText(applicationContext, title, Toast.LENGTH_SHORT).show()
-        }
+        getRequestWithQueryParameters()
+        getRequestWithPathParameters()
+    }
 
-
+    private fun getRequestWithQueryParameters() {
         val responseLiveDate: LiveData<Response<Albums>> = liveData {
             val response = retrofitService.getSortedAlbums(3)
             emit(response)
@@ -41,11 +38,22 @@ class MainActivity : AppCompatActivity() {
                 while (albumsList.hasNext()) {
                     val albumsItem = albumsList.next()
                     val result = " " + "Album Title: ${albumsItem.title}" + "\n" +
-                    " " + "Album id: ${albumsItem.id}" + "\n" +
-                    " " + "User id: ${albumsItem.userId}" + "\n\n\n"
+                            " " + "Album id: ${albumsItem.id}" + "\n" +
+                            " " + "User id: ${albumsItem.userId}" + "\n\n\n"
                     binding.textView.append(result)
                 }
             }
+        }
+    }
+
+    private fun getRequestWithPathParameters() {
+        val pathResponse: LiveData<Response<AlbumsItem>> = liveData {
+            val response = retrofitService.getAlbum(3)
+            emit(response)
+        }
+        pathResponse.observe(this) {
+            val title = it.body()?.title
+            Toast.makeText(applicationContext, title, Toast.LENGTH_SHORT).show()
         }
     }
 }
