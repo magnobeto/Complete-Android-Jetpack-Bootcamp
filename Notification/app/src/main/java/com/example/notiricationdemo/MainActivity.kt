452b493956eac04c0ad9cd964f75473d6e466cbf
Roberto Magno <roberto.magno@g.globo>
2022-06-07 23:51:26 -0300

    Implementando channel id e notification manager

--- Notification/app/src/main/java/com/example/notiricationdemo/MainActivity.kt
+++ Notification/app/src/main/java/com/example/notiricationdemo/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.notiricationdemo
 
+import android.app.NotificationManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.notiricationdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val channelID = "com.example.notiricationdemo.channel1"
+    private var notificationManager: NotificationManager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         binding.button.setOnClickListener {
             displayNotification()
