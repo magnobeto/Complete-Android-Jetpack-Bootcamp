0928cf4e890e56017380b6f08f09380e152b54f5
Roberto Magno <roberto.magno@g.globo>
2022-06-09 20:32:17 -0300

    Direct Reply Action message (dont working in android 12, check later)

--- Notification/app/src/main/java/com/example/notificationdemo/SecondActivity.kt
+++ Notification/app/src/main/java/com/example/notificationdemo/SecondActivity.kt
@@ -1,11 +1,28 @@
 package com.example.notificationdemo
 
+import android.app.RemoteInput
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notificationdemo.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        receiveInput()
+    }
+
+    private fun receiveInput() {
+        val KEY_REPLY = "key_reply"
+        val intent = this.intent
+        val remoteInput = RemoteInput.getResultsFromIntent(intent)
+        if (remoteInput != null) {
+            val inputString = remoteInput.getCharSequence(KEY_REPLY).toString()
+            binding.textSecondaryActivity.text = inputString
+        }
+
     }
 }
