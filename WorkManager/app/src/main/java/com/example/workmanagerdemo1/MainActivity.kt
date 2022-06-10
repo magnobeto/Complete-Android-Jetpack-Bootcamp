f39f457f4ca87276ffea2dab8462dbf0a39aa47d
Roberto Magno <roberto.magno@g.globo>
2022-06-09 21:23:06 -0300

    WorkManager One time work request example: set One Time Work Request

--- WorkManager/app/src/main/java/com/example/workmanagerdemo1/MainActivity.kt
+++ WorkManager/app/src/main/java/com/example/workmanagerdemo1/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.workmanagerdemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.example.workmanagerdemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.button.setOnClickListener {
+            setOneTimeWorkRequest()
+        }
+    }
+
+    private fun setOneTimeWorkRequest() {
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .build()
+        WorkManager.getInstance(applicationContext)
+            .enqueue(uploadRequest)
     }
 }
