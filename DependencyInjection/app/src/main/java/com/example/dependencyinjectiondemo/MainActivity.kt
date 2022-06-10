c5156f182ffa840de26dbbe673b9a4ac10428f71
Roberto Magno <roberto.magno@g.globo>
2022-06-10 18:27:45 -0300

    Field Injection with Dagger2

--- DependencyInjection/app/src/main/java/com/example/dependencyinjectiondemo/MainActivity.kt
+++ DependencyInjection/app/src/main/java/com/example/dependencyinjectiondemo/MainActivity.kt
@@ -2,17 +2,20 @@ package com.example.dependencyinjectiondemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var smartPhone: SmartPhone
+    @Inject
+    lateinit var smartPhone: SmartPhone
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         DaggerSmartPhoneComponent.create()
-            .getSmartPhone()
-            .makeACallWithRecording()
+            .inject(this)
+
+        smartPhone.makeACallWithRecording()
     }
 }
--- DependencyInjection/app/src/main/java/com/example/dependencyinjectiondemo/SmartPhoneComponent.kt
+++ DependencyInjection/app/src/main/java/com/example/dependencyinjectiondemo/SmartPhoneComponent.kt
@@ -4,5 +4,6 @@ import dagger.Component
 
 @Component(modules = [MemoryCardModule::class, NCBatteryModule::class])
 interface SmartPhoneComponent {
-    fun getSmartPhone(): SmartPhone
+
+    fun inject(mainActivity: MainActivity)
 }
