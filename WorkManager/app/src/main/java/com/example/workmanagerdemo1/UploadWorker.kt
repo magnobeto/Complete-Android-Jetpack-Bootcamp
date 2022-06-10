d6ed7300eedb615f94666bf6a4f5e26396e6f5b1
Roberto Magno <roberto.magno@g.globo>
2022-06-09 21:28:47 -0300

    Get status updates from workers

--- WorkManager/app/src/main/java/com/example/workmanagerdemo1/UploadWorker.kt
+++ WorkManager/app/src/main/java/com/example/workmanagerdemo1/UploadWorker.kt
@@ -9,7 +9,7 @@ import java.lang.Exception
 class UploadWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
     override fun doWork(): Result {
         return try {
-            for (i in 0..600) {
+            for (i in 0..600000) {
                 Log.i("MyTag", "Uploading $i")
             }
             Result.success()
