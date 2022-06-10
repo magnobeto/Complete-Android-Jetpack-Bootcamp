c99b06079e506d5aa9f9e8028df4a71c5b3b7b4b
Roberto Magno <roberto.magno@g.globo>
2022-06-09 21:14:55 -0300

    WorkManager One time work request example: implement doWork method

--- WorkManager/app/src/main/java/com/example/workmanagerdemo1/UploadWorker.kt
+++ WorkManager/app/src/main/java/com/example/workmanagerdemo1/UploadWorker.kt
@@ -1,11 +1,20 @@
 package com.example.workmanagerdemo1
 
 import android.content.Context
+import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import java.lang.Exception
 
 class UploadWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
     override fun doWork(): Result {
-        TODO("Not yet implemented")
+        return try {
+            for (i in 0..600) {
+                Log.i("MyTag", "Uploading $i")
+            }
+            Result.success()
+        } catch (e: Exception) {
+            Result.failure()
+        }
     }
 }
