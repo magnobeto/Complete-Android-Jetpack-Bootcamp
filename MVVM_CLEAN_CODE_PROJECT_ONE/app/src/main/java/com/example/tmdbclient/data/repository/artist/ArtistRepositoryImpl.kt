d3a94f89e06c99e6507acf712a02c2c976ac8415
Roberto Magno <roberto.magno@g.globo>
2022-06-19 22:02:54 -0300

    Create data layer part 4: Artist Repository final implementation

--- MVVM_CLEAN_CODE_PROJECT_ONE/app/src/main/java/com/example/tmdbclient/data/repository/artist/ArtistRepositoryImpl.kt
+++ MVVM_CLEAN_CODE_PROJECT_ONE/app/src/main/java/com/example/tmdbclient/data/repository/artist/ArtistRepositoryImpl.kt
@@ -1,15 +1,72 @@
 package com.example.tmdbclient.data.repository.artist
 
+import android.util.Log
 import com.example.tmdbclient.data.model.artist.Artist
+import com.example.tmdbclient.data.repository.artist.datasource.ArtistCacheDataSource
+import com.example.tmdbclient.data.repository.artist.datasource.ArtistLocalDataSource
+import com.example.tmdbclient.data.repository.artist.datasource.ArtistRemoteDataSource
 import com.example.tmdbclient.domain.repository.ArtistRepository
+import java.lang.Exception
 
-class ArtistRepositoryImpl: ArtistRepository {
+class ArtistRepositoryImpl(
+    private val artistRemoteDataSource: ArtistRemoteDataSource,
+    private val artistLocalDataSource: ArtistLocalDataSource,
+    private val artistCacheDataSource: ArtistCacheDataSource
+) : ArtistRepository {
 
-    override suspend fun getArtists(): List<Artist>? {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getArtists(): List<Artist>? = getArtistFromCache()
 
     override suspend fun updateArtists(): List<Artist>? {
-        TODO("Not yet implemented")
+        val newListOfMovies = getArtistsFromAPI()
+        artistLocalDataSource.clearAll()
+        artistLocalDataSource.saveArtistsToDB(newListOfMovies)
+        artistCacheDataSource.saveArtistsToCache(newListOfMovies)
+        return newListOfMovies
+    }
+
+    private suspend fun getArtistsFromAPI(): List<Artist> {
+        lateinit var artistList: List<Artist>
+        try {
+            val response = artistRemoteDataSource.getArtists()
+            val body = response.body()
+            if (body != null) {
+                artistList = body.artists
+            }
+        } catch (exception: Exception) {
+            Log.i("MyTag", exception.message.toString())
+        }
+        return artistList
+    }
+
+    private suspend fun getArtistsFromDB(): List<Artist> {
+        lateinit var artistList: List<Artist>
+        try {
+            artistList = artistLocalDataSource.getArtistsFromDB()
+        } catch (exception: Exception) {
+            Log.i("MyTag", exception.message.toString())
+        }
+        if (artistList.isNotEmpty()) {
+            return artistList
+        } else {
+            artistList = getArtistsFromAPI()
+            artistLocalDataSource.saveArtistsToDB(artistList)
+        }
+        return artistList
+    }
+
+    private suspend fun getArtistFromCache(): List<Artist> {
+        lateinit var artistList: List<Artist>
+        try {
+            artistList = artistCacheDataSource.getArtistsFromCache()
+        } catch (exception: Exception) {
+            Log.i("MyTag", exception.message.toString())
+        }
+        if (artistList.isNotEmpty()) {
+            return artistList
+        } else {
+            artistList = getArtistsFromDB()
+            artistCacheDataSource.saveArtistsToCache(artistList)
+        }
+        return artistList
     }
 }
