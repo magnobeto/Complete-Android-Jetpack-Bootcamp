56a76e74c58ac63e316303c0627aef7e1eecf086
roberto.magno <roberto.magno@robertomagno.local>
2022-06-23 19:50:32 -0300

    Create presentation layer part 3: creating database dependency injection module with daggger

---
+++ MVVM_CLEAN_CODE_PROJECT_ONE/app/src/main/java/com/example/tmdbclient/presentation/di/DataBaseModule.kt
@@ -0,0 +1,35 @@
+package com.example.tmdbclient.presentation.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.tmdbclient.data.db.ArtistDao
+import com.example.tmdbclient.data.db.MovieDao
+import com.example.tmdbclient.data.db.TMDBDatabase
+import com.example.tmdbclient.data.db.TvShowDao
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class DataBaseModule {
+    @Singleton
+    @Provides
+    fun provideMovieDataBase(context: Context): TMDBDatabase {
+        return Room.databaseBuilder(context, TMDBDatabase::class.java, "tmdbclient").build()
+    }
+    @Singleton
+    @Provides
+    fun provideMovieDao(tmdbDatabase: TMDBDatabase): MovieDao {
+        return tmdbDatabase.movieDao()
+    }
+    @Singleton
+    @Provides
+    fun provideTvDao(tmdbDatabase: TMDBDatabase): TvShowDao {
+        return tmdbDatabase.tvDao()
+    }
+    @Singleton
+    @Provides
+    fun provideArtistDao(tmdbDatabase: TMDBDatabase): ArtistDao {
+        return tmdbDatabase.artistDao()
+    }
+}
