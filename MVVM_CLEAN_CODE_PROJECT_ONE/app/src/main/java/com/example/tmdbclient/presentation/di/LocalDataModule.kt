ce75227865d33eec20f098b9fcb9c59548ceca3c
roberto.magno <roberto.magno@robertomagno.local>
2022-06-23 20:06:19 -0300

    Create presentation layer part 3: creating localdata dependency injection module with daggger

---
+++ MVVM_CLEAN_CODE_PROJECT_ONE/app/src/main/java/com/example/tmdbclient/presentation/di/LocalDataModule.kt
@@ -0,0 +1,35 @@
+package com.example.tmdbclient.presentation.di
+
+import com.example.tmdbclient.data.db.ArtistDao
+import com.example.tmdbclient.data.db.MovieDao
+import com.example.tmdbclient.data.db.TvShowDao
+import com.example.tmdbclient.data.repository.artist.datasource.ArtistLocalDataSource
+import com.example.tmdbclient.data.repository.artist.datasourceimpl.ArtistLocalDataSourceImpl
+import com.example.tmdbclient.data.repository.movie.datasource.MovieLocalDataSource
+import com.example.tmdbclient.data.repository.movie.datasourceimpl.MovieLocalDataSourceImpl
+import com.example.tmdbclient.data.repository.tvshow.datasource.TvShowLocalDataSource
+import com.example.tmdbclient.data.repository.tvshow.datasourceimpl.TvShowLocalDataSourceImpl
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class LocalDataModule {
+    @Singleton
+    @Provides
+    fun provideMovieLocalDataSource(movieDao: MovieDao): MovieLocalDataSource {
+        return MovieLocalDataSourceImpl(movieDao)
+    }
+
+    @Singleton
+    @Provides
+    fun provideTvShowLocalDataSource(tvShowDao: TvShowDao): TvShowLocalDataSource {
+        return TvShowLocalDataSourceImpl(tvShowDao)
+    }
+
+    @Singleton
+    @Provides
+    fun provideArtistLocalDataSource(artistDao: ArtistDao): ArtistLocalDataSource {
+        return ArtistLocalDataSourceImpl(artistDao)
+    }
+}
