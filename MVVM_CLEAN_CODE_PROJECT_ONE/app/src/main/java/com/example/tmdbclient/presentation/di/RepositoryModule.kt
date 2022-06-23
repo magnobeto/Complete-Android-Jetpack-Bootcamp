d8d4640cba08f669ab14cc4439423a4fa8c57379
roberto.magno <roberto.magno@robertomagno.local>
2022-06-23 20:18:45 -0300

    Create presentation layer part 3: creating repository dependency injection module with daggger

---
+++ MVVM_CLEAN_CODE_PROJECT_ONE/app/src/main/java/com/example/tmdbclient/presentation/di/RepositoryModule.kt
@@ -0,0 +1,63 @@
+package com.example.tmdbclient.presentation.di
+
+import com.example.tmdbclient.data.repository.artist.ArtistRepositoryImpl
+import com.example.tmdbclient.data.repository.artist.datasource.ArtistCacheDataSource
+import com.example.tmdbclient.data.repository.artist.datasource.ArtistLocalDataSource
+import com.example.tmdbclient.data.repository.artist.datasource.ArtistRemoteDataSource
+import com.example.tmdbclient.data.repository.movie.MovieRepositoryImpl
+import com.example.tmdbclient.data.repository.movie.datasource.MovieCacheDataSource
+import com.example.tmdbclient.data.repository.movie.datasource.MovieLocalDataSource
+import com.example.tmdbclient.data.repository.movie.datasource.MovieRemoteDataSource
+import com.example.tmdbclient.data.repository.tvshow.TvShowRepositoryImpl
+import com.example.tmdbclient.data.repository.tvshow.datasource.TvShowCacheDataSource
+import com.example.tmdbclient.data.repository.tvshow.datasource.TvShowLocalDataSource
+import com.example.tmdbclient.data.repository.tvshow.datasource.TvShowRemoteDataSource
+import com.example.tmdbclient.domain.repository.ArtistRepository
+import com.example.tmdbclient.domain.repository.MovieRepository
+import com.example.tmdbclient.domain.repository.TvShowRepository
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class RepositoryModule {
+    @Singleton
+    @Provides
+    fun provideMovieRepository(
+        movieRemoteDataSource: MovieRemoteDataSource,
+        movieLocalDataSource: MovieLocalDataSource,
+        movieCacheDataSource: MovieCacheDataSource
+    ): MovieRepository {
+        return MovieRepositoryImpl(
+            movieRemoteDataSource,
+            movieLocalDataSource,
+            movieCacheDataSource
+        )
+    }
+    @Singleton
+    @Provides
+    fun provideTvShowRepository(
+        tvShowRemoteDataSource: TvShowRemoteDataSource,
+        tvShowLocalDataSource: TvShowLocalDataSource,
+        tvShowCacheDataSource: TvShowCacheDataSource
+    ): TvShowRepository {
+        return TvShowRepositoryImpl(
+            tvShowRemoteDataSource,
+            tvShowLocalDataSource,
+            tvShowCacheDataSource
+        )
+    }
+    @Singleton
+    @Provides
+    fun provideArtistRepository(
+        artistRemoteDataSource: ArtistRemoteDataSource,
+        artistLocalDataSource: ArtistLocalDataSource,
+        artistCacheDataSource: ArtistCacheDataSource
+    ): ArtistRepository {
+        return ArtistRepositoryImpl(
+            artistRemoteDataSource,
+            artistLocalDataSource,
+            artistCacheDataSource
+        )
+    }
+}
