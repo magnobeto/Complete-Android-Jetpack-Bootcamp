be63e2c54c9427fd872a64a1def3e68b4ede6678
roberto.magno <roberto.magno@robertomagno.local>
2022-06-23 19:58:42 -0300

    Create presentation layer part 3: creating remotedata dependency injection module with daggger

---
+++ MVVM_CLEAN_CODE_PROJECT_ONE/app/src/main/java/com/example/tmdbclient/presentation/di/RemoteDataModule.kt
@@ -0,0 +1,33 @@
+package com.example.tmdbclient.presentation.di
+
+import com.example.tmdbclient.data.api.TMDBService
+import com.example.tmdbclient.data.repository.artist.datasource.ArtistRemoteDataSource
+import com.example.tmdbclient.data.repository.artist.datasourceimpl.ArtistRemoteDataSourceImpl
+import com.example.tmdbclient.data.repository.movie.datasource.MovieRemoteDataSource
+import com.example.tmdbclient.data.repository.movie.datasourceimpl.MovieRemoteDataSourceImpl
+import com.example.tmdbclient.data.repository.tvshow.datasource.TvShowRemoteDataSource
+import com.example.tmdbclient.data.repository.tvshow.datasourceimpl.TvShowRemoteDataSourceImpl
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class RemoteDataModule(private val apiKey: String) {
+    @Singleton
+    @Provides
+    fun provideMovieRemoteDataSource(tmdbService: TMDBService): MovieRemoteDataSource {
+        return MovieRemoteDataSourceImpl(tmdbService, apiKey)
+    }
+
+    @Singleton
+    @Provides
+    fun provideTvRemoteDataSource(tmdbService: TMDBService): TvShowRemoteDataSource {
+        return TvShowRemoteDataSourceImpl(tmdbService, apiKey)
+    }
+
+    @Singleton
+    @Provides
+    fun provideArtistRemoteDataSource(tmdbService: TMDBService): ArtistRemoteDataSource {
+        return ArtistRemoteDataSourceImpl(tmdbService, apiKey)
+    }
+}
