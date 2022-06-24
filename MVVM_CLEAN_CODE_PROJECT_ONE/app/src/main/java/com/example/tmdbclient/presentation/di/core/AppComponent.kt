b865d5cd8baab8bc9a8c61ddddac3366d922115e
roberto.magno <roberto.magno@robertomagno.local>
2022-06-23 21:07:24 -0300

    Refactor Appmodule andr Appcomponent

--- MVVM_CLEAN_CODE_PROJECT_ONE/app/src/main/java/com/example/tmdbclient/presentation/di/core/AppComponent.kt
+++ MVVM_CLEAN_CODE_PROJECT_ONE/app/src/main/java/com/example/tmdbclient/presentation/di/core/AppComponent.kt
@@ -1,6 +1,10 @@
 package com.example.tmdbclient.presentation.di.core
 
+import com.example.tmdbclient.presentation.di.artist.ArtistSubComponent
+import com.example.tmdbclient.presentation.di.movie.MovieSubComponent
+import com.example.tmdbclient.presentation.di.tvshow.TvShowSubComponent
 import dagger.Component
+import dagger.Subcomponent
 import javax.inject.Singleton
 
 @Singleton
@@ -16,5 +20,7 @@ import javax.inject.Singleton
     ]
 )
 interface AppComponent {
-
+    fun movieSubComponent(): MovieSubComponent.Factory
+    fun tvShowSubComponent(): TvShowSubComponent.Factory
+    fun artistSubComponent(): ArtistSubComponent.Factory
 }
--- MVVM_CLEAN_CODE_PROJECT_ONE/app/src/main/java/com/example/tmdbclient/presentation/di/core/AppModule.kt
+++ MVVM_CLEAN_CODE_PROJECT_ONE/app/src/main/java/com/example/tmdbclient/presentation/di/core/AppModule.kt
@@ -1,11 +1,20 @@
 package com.example.tmdbclient.presentation.di.core
 
 import android.content.Context
+import com.example.tmdbclient.presentation.di.artist.ArtistSubComponent
+import com.example.tmdbclient.presentation.di.movie.MovieSubComponent
+import com.example.tmdbclient.presentation.di.tvshow.TvShowSubComponent
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
-@Module
+@Module(
+    subcomponents = [
+        MovieSubComponent::class,
+        TvShowSubComponent::class,
+        ArtistSubComponent::class
+    ]
+)
 class AppModule(private val context: Context) {
     @Singleton
     @Provides
