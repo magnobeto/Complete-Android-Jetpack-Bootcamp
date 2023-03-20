9d5aa1a49bf007e23d05242f71114596ab937cf6
roberto.magno <roberto.magno@g.globo>
2023-03-19 22:25:33 -0300

    Escrevendo primeiros testes para o método de cálculo de área

--- Unit-Test-Fundamentals/CircleCalc/app/src/test/java/com/anushka/circlecalc/MyCalcTest.kt
+++ Unit-Test-Fundamentals/CircleCalc/app/src/test/java/com/anushka/circlecalc/MyCalcTest.kt
@@ -14,14 +14,26 @@ class MyCalcTest {
     }
 
     @Test
-    fun `GIVEN a valid radius value WHEN calculate circunference THEN return correct result`() {
+    fun `GIVEN a valid radius value WHEN calculate circumference THEN return correct result`() {
         val result = myCalc.calculateCircumference(2.1)
         result shouldBeEqualTo 13.188
     }
 
     @Test
-    fun `GIVEN a radius value equals zero WHEN calculate circunference THEN return correct result`() {
+    fun `GIVEN a radius value equals zero WHEN calculate circumference THEN return correct result`() {
         val result = myCalc.calculateCircumference(0.0)
+        result shouldBeEqualTo 0.0
+    }
+
+    @Test
+    fun `GIVEN a valid radius value WHEN calculate area THEN return correct result`() {
+        val result = myCalc.calculateArea(2.0)
+        result shouldBeEqualTo 12.56
+    }
+
+    @Test
+    fun `GIVEN a radius value equals zero WHEN calculate area THEN return correct result`() {
+        val result = myCalc.calculateArea(0.0)
         result shouldBeEqualTo 0.0
     }
 }
