683eb24583a5c0d1f0681dd58fd05f8333ceeea8
roberto.magno <roberto.magno@g.globo>
2023-04-09 12:37:35 -0300

    Implementando fragments e navigation components

--- MVVM_CLEAN_CODE_PROJECT_TWO/app/src/main/java/com/example/appnews/presentation/MainActivity.kt
+++ MVVM_CLEAN_CODE_PROJECT_TWO/app/src/main/java/com/example/appnews/presentation/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.appnews.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.appnews.R
+import com.example.appnews.databinding.ActivityMainBinding
 import com.example.appnews.presentation.viewmodel.NewsViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val newsViewModel: NewsViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
