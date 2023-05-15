e83ae16e2bb88c05f7422afb3626c8e262c6cf56
roberto.magno <roberto.magno@g.globo>
2023-05-14 23:57:06 -0300

    Testing modifiers and other parameters

--- Jetpack-Compose-Fundamentals/ComposeDemo1/app/src/main/java/com/example/composedemo1/MainActivity.kt
+++ Jetpack-Compose-Fundamentals/ComposeDemo1/app/src/main/java/com/example/composedemo1/MainActivity.kt
@@ -3,9 +3,21 @@ package com.example.composedemo1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composedemo1.ui.theme.ComposeDemo1Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +31,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.Blue,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(color = Color.Yellow)
+            .border(2.dp, Color.Green)
+            .padding(10.dp)
+            .fillMaxWidth(0.5f)
+            .fillMaxHeight(0.3f)
+    )
 }
 
 @Preview(showBackground = true)
