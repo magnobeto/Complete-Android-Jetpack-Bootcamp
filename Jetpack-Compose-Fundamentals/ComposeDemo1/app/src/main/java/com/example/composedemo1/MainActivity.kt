0459fe7fb6e39d131bd14c4935c962764e6e526b
roberto.magno <roberto.magno@g.globo>
2023-05-14 23:40:26 -0300

    Initial setup project example for compose

---
+++ Jetpack-Compose-Fundamentals/ComposeDemo1/app/src/main/java/com/example/composedemo1/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.composedemo1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composedemo1.ui.theme.ComposeDemo1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeDemo1Theme {
+        Greeting("Android")
+    }
+}
