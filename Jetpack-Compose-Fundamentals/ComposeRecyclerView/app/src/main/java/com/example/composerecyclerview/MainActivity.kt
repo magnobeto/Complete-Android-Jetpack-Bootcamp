790d5ace0be4402842c7808cd7c3e4b6b987acdf
roberto.magno <roberto.magno@g.globo>
2023-05-21 20:43:12 -0300

    Iniciando projeto de aprendizado lazy column(Recyclerview)

---
+++ Jetpack-Compose-Fundamentals/ComposeRecyclerView/app/src/main/java/com/example/composerecyclerview/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.composerecyclerview
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composerecyclerview.ui.theme.ComposeRecyclerViewTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeRecyclerViewTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
