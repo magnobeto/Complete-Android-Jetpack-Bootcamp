47c818d73280fc3f6d0a1fc2073ab4bc30c5160c
roberto.magno <roberto.magno@g.globo>
2023-05-21 14:42:36 -0300

    Testing Box with modifiers

--- Jetpack-Compose-Fundamentals/ComposeDemo1/app/src/main/java/com/example/composedemo1/MainActivity.kt
+++ Jetpack-Compose-Fundamentals/ComposeDemo1/app/src/main/java/com/example/composedemo1/MainActivity.kt
@@ -3,35 +3,30 @@ package com.example.composedemo1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.composedemo1.ui.theme.ComposeDemo1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                modifier = Modifier
-                    .background(color = Color.Gray)
-                    .fillMaxSize(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Greeting("AB")
-                Greeting("CDEF")
-                Greeting("G")
-            }
+            BoxExample3()
         }
     }
 }
@@ -51,10 +46,143 @@ fun Greeting(name: String) {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeDemo1Theme {
-        Greeting("Android")
+fun BoxExample1() {
+    Box(
+        modifier = Modifier
+            .background(Color.Cyan)
+            .size(180.dp, 300.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color.Green)
+                .size(125.dp, 100.dp)
+                .align(Alignment.TopEnd)
+        ) {
+
+        }
+
+        Text(
+            text = "Hi",
+            style = MaterialTheme.typography.h3,
+            modifier = Modifier
+                .background(Color.White)
+                .size(90.dp, 50.dp)
+                .align(Alignment.BottomCenter)
+        )
+    }
+}
+
+@Composable
+fun BoxExample2() {
+    Box(
+        modifier = Modifier
+            .background(color = Color.LightGray)
+            .fillMaxSize()
+    ) {
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(Color.Magenta)
+                .padding(10.dp)
+                .align(Alignment.TopStart),
+            text = "TopStart"
+        )
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(Color.Magenta)
+                .padding(10.dp)
+                .align(Alignment.TopCenter),
+            text = "TopCenter"
+        )
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(Color.Magenta)
+                .padding(10.dp)
+                .align(Alignment.TopEnd),
+            text = "TopEnd"
+        )
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(Color.Magenta)
+                .padding(10.dp)
+                .align(Alignment.CenterStart),
+            text = "CenterStart"
+        )
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(Color.Magenta)
+                .padding(10.dp)
+                .align(Alignment.Center),
+            text = "Center"
+        )
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(Color.Magenta)
+                .padding(10.dp)
+                .align(Alignment.CenterEnd),
+            text = "CenterEnd"
+        )
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(Color.Magenta)
+                .padding(10.dp)
+                .align(Alignment.BottomStart),
+            text = "BottomStart"
+        )
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(Color.Magenta)
+                .padding(10.dp)
+                .align(Alignment.BottomCenter),
+            text = "BottomCenter"
+        )
+        Text(
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(Color.Magenta)
+                .padding(10.dp)
+                .align(Alignment.BottomEnd),
+            text = "BottomEnd"
+        )
+    }
+}
+
+@Composable
+fun BoxExample3() {
+    Box {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Description",
+            modifier = Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(400.dp)
+        )
+        Text(
+            text = "Image Description",
+            style = MaterialTheme.typography.h4,
+            modifier = Modifier.align(Alignment.BottomStart)
+        )
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.textButtonColors(
+                backgroundColor = Color.White,
+                contentColor = Color.DarkGray
+            ),
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(10.dp)
+                .border(5.dp, Color.DarkGray, RectangleShape)
+        ) {
+            Text("Add to cart")
+        }
     }
 }
