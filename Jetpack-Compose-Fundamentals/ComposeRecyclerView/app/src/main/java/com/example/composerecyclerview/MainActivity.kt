33470abcf04131b3de16b3ffecfa0a8954109418
roberto.magno <roberto.magno@g.globo>
2023-05-21 20:59:59 -0300

    Implementação equivalente ao listview

--- Jetpack-Compose-Fundamentals/ComposeRecyclerView/app/src/main/java/com/example/composerecyclerview/MainActivity.kt
+++ Jetpack-Compose-Fundamentals/ComposeRecyclerView/app/src/main/java/com/example/composerecyclerview/MainActivity.kt
@@ -3,27 +3,46 @@ package com.example.composerecyclerview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composerecyclerview.ui.theme.ComposeRecyclerViewTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeRecyclerViewTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
+            ScrollableColumnDemo1()
+        }
+    }
+}
 
-                }
-            }
+@Preview(showBackground = true)
+@Composable
+fun ScrollableColumnDemo1() {
+    // Equivalent to ListView, not ideal for large amount of items
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier.verticalScroll(scrollState)
+    ) {
+        for (i in 1..100) {
+            Text(
+                text = "User Name: $i",
+                style = MaterialTheme.typography.h3,
+                modifier = Modifier.padding(10.dp)
+            )
+            Divider(color = Color.Black, thickness = 5.dp)
         }
     }
 }
