5606407811be4f82fcefeefcb66c1538b22f44a5
roberto.magno <roberto.magno@g.globo>
2023-05-28 15:45:23 -0300

    Buffer and CollectLatest

--- Jetpack-Compose-Fundamentals/FlowDemo/app/src/main/java/com/example/flowdemo/MyViewModel.kt
+++ Jetpack-Compose-Fundamentals/FlowDemo/app/src/main/java/com/example/flowdemo/MyViewModel.kt
@@ -1,15 +1,59 @@
 package com.example.flowdemo
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
-class MyViewModel: ViewModel() {
+class MyViewModel : ViewModel() {
 
     val myFlow = flow<Int> {
         for (i in 1..100) {
             emit(i)
             delay(1000L)
+        }
+    }
+
+    init {
+        backPressureDemo()
+    }
+
+    private fun backPressureDemo() {
+        val myflow1 = flow<Int> {
+            for (i in 1..10) {
+                Log.i("MYTAG", "Produced $i")
+                emit(i)
+                delay(1000L)
+            }
+        }
+
+        // Only Produce again when a previous value has been consumed, cause gap time
+//        viewModelScope.launch {
+//            myflow1.collect {
+//                delay(2000L)
+//                Log.i("MYTAG", "Consumed $it")
+//            }
+//        }
+
+        // Alow to buffer values produced, to be consume on the time by consumer
+//        viewModelScope.launch {
+//            myflow1.buffer().collect {
+//                delay(2000L)
+//                Log.i("MYTAG", "Consumed $it")
+//            }
+//        }
+
+        // Consume only latest value produced
+        viewModelScope.launch {
+            myflow1.collectLatest {
+                delay(2000L)
+                Log.i("MYTAG", "Consumed $it")
+            }
         }
     }
 }
